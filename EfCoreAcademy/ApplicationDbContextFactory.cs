eeb31e232800af3303605e5dd04f76ff3e5eb0e3
taufancoder <taufancoder@gmail.com>
2023-05-27 17:51:42 +0700

    Code First

---
+++ EfCoreAcademy/ApplicationDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace EfCoreAcademy;
+
+public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+        optionsBuilder.UseSqlite("Filename=EfCoreAcademy.db");
+
+        return new ApplicationDbContext(optionsBuilder.Options);
+    }
+}
--- EfCoreAcademy/Program.cs
+++ EfCoreAcademy/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Installing EF Core tools: tool install --global dotnet-ef
+//Creating migrations: dotnet ef migrations add InitialMigration
+
+using EfCoreAcademy;
+using Microsoft.EntityFrameworkCore;
+
+var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseSqlite().Options;
+var dbContext = new ApplicationDbContext(options);
+
+dbContext.Database.Migrate();
